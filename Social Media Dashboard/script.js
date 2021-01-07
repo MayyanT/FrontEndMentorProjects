e0b4d91d4d7019ab0b332caf644ef8d10ed95856
Mayyan To <63452466+MayyanT@users.noreply.github.com>
2021-01-07 12:00:26 +0000

    Update script.js

--- Social Media Dashboard/script.js
+++ Social Media Dashboard/script.js
@@ -1,5 +1,4 @@
 const checkbox = document.getElementById('checkbox');
-const mode = document.getElementById('mode');
 
 checkbox.addEventListener('change', () => {
     document.body.classList.toggle('active')
